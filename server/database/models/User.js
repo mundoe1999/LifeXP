5cfe121a2c8b49432e698bac9166aba5115ab4bc
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2019-06-14 14:58:40 -0400

    db definitions

---
+++ server/database/models/User.js
@@ -0,0 +1,37 @@
+const db = require('../database');
+const Sequelize = require('sequelize');
+
+const User = db.define('users', {
+  fname: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lname: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  user: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  image: {
+    type: Sequelize.STRING,
+    allowNull: true,
+    defaultValue: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKRsW4WE_L7W_VkeJ2YXQEotSsPGDXbwPDhe_anhDdtyehO6bSjw'
+  },
+  level: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: 'EASY'
+  }
+});
+
+module.exports = User;

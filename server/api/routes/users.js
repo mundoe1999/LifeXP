69cfbed5de5ed4af5dcca3362454311bfdf71f7e
mundoe1999 <estebanmundo1999@gmail.com>
2019-06-16 17:17:27 -0400

    Adding Routing

--- server/api/routes/users.js
+++ server/api/routes/users.js
@@ -1,23 +1,55 @@
 const router = require('express').Router();
+const { User, Task, Board } = require('../../database');
 
 /*******************************************************
  * Getting state of user table
 ********************************************************/
 
 router.get('/', async(req, res, next) =>{
-    res.send('all Users');
+  try{
+    const allUsers = await User.findAll();
+    res.send(allUsers);
+  } catch(error){
+    console.log("Unable to get Users");
+  }
 });
 
 router.get('/:userId', async(req,res,next) =>{
-    res.send(`Selecting User ${req.params.userId}`)
+  try{
+    const user = await User.findByPk(req.params.userId);
+    res.send(user);
+  } catch(error){
+    console.log(`Unable to get User ${req.params.userId}`);
+  }
 });
 
 router.get('/:userId/tasks', async(req,res,next) =>{
-  res.send(`Selecting all tasks attached to ${req.params.userId}`)
+  try{
+    const allTasksFromUser = Task.findAll({
+      where: {
+        userId: req.params.userId
+      }
+    });
+    res.send(allTasksFromUser);
+  } catch(error){
+    console.log(`Unable to get User ${req.params.userId}'s Tasks`);
+  }
+
 });
 
 router.get('/:userId/boards', async(req,res,next) =>{
-  res.send(`Selecting all boards attached to ${req.params.userId}`)
+  /*
+  try{
+    const allBoardsFromUser = Board.findAll({
+      where: {
+        userId: req.params.userId
+      }
+    });
+    res.send(allTasksFromUser);
+  } catch(error){
+    console.log(`Unable to get User ${req.params.userId}'s Tasks`);
+  }
+  */
 });
 
 /*******************************************************

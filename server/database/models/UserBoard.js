b54b1b4eb916d8cb0770e9e14a7512f06f102607
mundoe1999 <estebanmundo1999@gmail.com>
2019-06-17 01:16:22 -0400

    Updated UserBoard.js

--- server/database/models/UserBoard.js
+++ server/database/models/UserBoard.js
@@ -5,12 +5,18 @@ const UserBoard = db.define('userBoard', {
   userId: {
     type: Sequelize.INTEGER,
     allowNull: false,
-
+    references: {
+      model: 'User',
+      key: 'id'
+    }
   },
   boardId: {
     type: Sequelize.INTEGER,
     allowNull: false,
-
+    references: {
+      model: 'Board',
+      key: 'id'
+    }
   }
 });
 

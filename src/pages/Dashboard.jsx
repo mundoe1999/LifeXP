805b774899171b1f00d01d3187518874cfc0a3d8
Tommi-Tsuruga <tommi.tsuruga@gmail.com>
2019-06-17 15:59:37 -0400

    massive refactor

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,21 +2,11 @@ import React, { Component } from 'react';
 import NavBar from '../components/essentials/NavBar';
 import TitleDesc from '../components/dashboard/TitleDesc';
 import BoardCard from '../components/dashboard/BoardCard'
-import {fetchBoards} from '../actions/boardActions';
 
 class Project extends Component {
 
-	componentWillMount() {
-		this.props.fetchBoards();
-	}
 
 	render() {
-		const boardItems = this.props.boards.map(board => (
-			<div key={board.boardId}>
-				<h3>{board.name}</h3>
-				<h3>{board.desc}</h3>
-			</div>
-		))
 		return (
 			<div>
 				<head>
@@ -27,7 +17,6 @@ class Project extends Component {
 					<div className="DashboardPad">
 						<TitleDesc />
 						<div class="ScrollingFlexWrapper DisableScrollbars">
-							{boardItems}
 							<BoardCard /><BoardCard /><BoardCard /><BoardCard /><BoardCard /><BoardCard /><BoardCard /><BoardCard /><BoardCard /><BoardCard />
 						</div>
 					</div>

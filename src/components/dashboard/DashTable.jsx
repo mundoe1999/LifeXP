98a92dcf0f30b922ec728f811adc587c386bd69b
Gareth <gareth.petterson70@myhunter.cuny.edu>
2019-06-19 12:53:03 -0400

    added modal component

--- src/components/dashboard/DashTable.jsx
+++ src/components/dashboard/DashTable.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
+import Modal from '../essentials/Modal';
 //import { connect } from 'react-redux';
 
-const DashTable = (props) => (
-	<table>
-		<thead>
-			<tr>
-				<th>board</th>
-				<th>to-do</th>
-				<th>Difficulty</th>
-				<th>status</th>
-				<th></th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<th>Robinson Family</th>
-				<th>Clean the Floors</th>
-				<th>Hard</th>
-				<th>Not Started</th>
-				<th>Edit</th>
-			</tr>
-		</tbody>
-	</table>
-);
+class DashTable extends React.Component{
+	constructor(props){
+		super(props);
+	}
 
+	render(){
+	return(
+	<div>
+		<table>
+			<thead>
+				<tr>
+					<th>board</th>
+					<th>to-do</th>
+					<th>Difficulty</th>
+					<th>status</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th>Robinson Family</th>
+					<th>Clean the Floors</th>
+					<th>Hard</th>
+					<th>Not Started</th>
+				</tr>
+			</tbody>
+		</table>
+		<Modal/>
+	</div>
+	)}
+}
 
 export default DashTable;

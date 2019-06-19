efa2b33193114058426ff384b0fd521ffed6b717
Tommi Tsuruga <tommi.tsuruga@gmail.com>
2019-06-19 19:13:23 -0400

    added some redux comments but don't know what to say

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 //index.js contains all the exports for the reducer folder. Calling this
 //or the root folder will export each respecrtive reducer in this folder
-
+//is exported in to the store as a combined reducer
 import { combineReducers } from 'redux';
 import boardReducer from './boardReducer';
 import taskReducer from './taskReducer';
--- src/store.js
+++ src/store.js
@@ -2,10 +2,17 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
+//Initial state for setting up the store. Initially an empty object
 const initialState = {};
 
+
+/*array of middlewear, currently only contains thunk which allows
+the action creators to return a function as an action
+*/
 const middleware = [thunk];
 
+//The store contains the root reducer exported from ./reducers/index.js
+//the initial state, and the middlware
 const store = createStore(
   rootReducer,
   initialState,

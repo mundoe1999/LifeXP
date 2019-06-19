eccca4a6fa74aaa5d42e650385ddf3dbc2c06616
Tommi-Tsuruga <tommi.tsuruga@gmail.com>
2019-06-19 11:57:41 -0400

    typo fix

--- src/components/dashboard/DashTable.jsx
+++ src/components/dashboard/DashTable.jsx
@@ -1,3 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+class DashTable extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			data: ' '
+		};
+	}
+
+	render() {
+		const tasks = this.props.tasks.map((currentTask) =>
+			<tr>
+			<th>{currentTask.name}</th>
+			<th>{currentTask.desc}</th>
+			<th></th>
+			<th> {currentTask.status}</th>
+			</tr>
+		)
+		return (
+			<div>
+				<table>
+					<thead>
+						<tr>
+							<th>board</th>
+							<th>to-do</th>
+							<th>level</th>
+							<th>status</th>
+						</tr>
+					</thead>
+					<tbody>
+					{tasks}
+					</tbody>
+
+				</table>
+			</div >
+		)
+	}
+}
+
+export default DashTable;
+
+
+
 /*					<table>
  						<tr>
     						<th>board</th>
@@ -47,47 +93,3 @@
             			})
         			}
 */
-
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-
-class DashTable extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			data: ' '
-		};
-	}
-
-	render() {
-		const tasks = this.props.tasks.map((currentTask) =>
-			<tr>
-			<th>{currentTask.name}</th>
-			<th>{currentTask.desc}</th>
-			<th></th>
-			<th> {currentTask.status}</th>
-			</tr>
-		)
-		return (
-			<div>
-				<table>
-					<thead>
-						<tr>
-							<th>board</th>
-							<th>to-do</th>
-							<th>level</th>
-							<th>status</th>
-						</tr>
-					</thead>
-					<tbody>
-					{tasks}
-					</tbody>
-
-				</table>
-			</div >
-		)
-	}
-}
-
-export default DashTable;
--- src/components/essentials/taskFormShell.jsx
+++ src/components/essentials/taskFormShell.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-\import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 import { addNewTaskThunk } from '../../actions/taskActions';
 
 class NewTaskForm extends Component {

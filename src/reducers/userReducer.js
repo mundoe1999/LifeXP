2aea188981b65bedcfded2b4c3de8fddd4a307b1
Tommi Tsuruga <tommi.tsuruga@gmail.com>
2019-06-19 19:15:28 -0400

    added some redux comments but don't know what to say

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,6 +1,8 @@
 
 
 import { FETCH_USERS, ADD_NEW_USER, FETCH_USER, REMOVE_USER } from '../actions/types';
+
+//initial state for the actions is empty, use this to store payload
 const initialState = {
   items: [],
   item: {}
@@ -9,7 +11,7 @@ const initialState = {
 
 /*
 User Reducer: Reads the action type and executes some non-mutating
-data processing. Returns a copy of a new state to 
+data processing. Returns a copy of a new state to allow modification of the store
 */
 export default function (state = initialState, action) {
   switch (action.type) {

19ef58cb55af504f695b8339d3c3f95291e3f9a5
mundoe1999 <estebanmundo1999@gmail.com>
2019-06-20 17:00:34 -0400

    Angry

--- server/database/database.js
+++ server/database/database.js
@@ -1,7 +1,4 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize({
-  connectionString:
-    process.env.DATABASE_URL || 'postgres://postgres:123@localhost:3030/Board-Manager'
-}); 
+const db = new Sequelize((process.env.DATABASE_URL || 'postgres://postgres:123@localhost:3030/Board-Manager')); 
 
 module.exports = db;

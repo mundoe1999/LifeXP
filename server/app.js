58c005bcc69c9a560c9b891e1fa4c5f65a9e9e0a
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2019-06-20 12:13:09 -0400

    package update for deployment

--- server/app.js
+++ server/app.js
@@ -1,14 +1,19 @@
+const path = require("path);")
 const express = require('express');
 const bodyParser = require('body-parser');
 const {db} = require('./database');
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Setting up routing
 app.use(bodyParser.json());
 app.use('/api', require('./api'));
 
+let locationOfPublicFolder = path.join(__dirname, "client", "build");
+app.use(express.static(locationOfPublicFolder));
+
 // Listener at port 8080
 db.sync()
   .then(() => {
-    app.listen(8080,() => console.log('Connected at 8080'));
+    app.listen(PORT,() => console.log('Connected at ${PORT}'));
   });

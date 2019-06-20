4acf837109d35ac0cca8b6235e1c99f8af1393db
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2019-06-20 15:23:57 -0400

    db setup

--- server/database/database.js
+++ server/database/database.js
@@ -2,6 +2,6 @@ const Sequelize = require('sequelize');
 const db = new Sequelize({
   connectionString:
     process.env.DATABASE_URL || 'postgres://postgres:123@localhost:3030/Board-Manager'
-});
+}); 
 
 module.exports = db;

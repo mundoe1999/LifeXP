aff7f4dc584d252ed26d3d69356041cf23b56116
Tommi-Tsuruga <tommi.tsuruga@gmail.com>
2019-06-20 17:13:18 -0400

    working server port in database.js for tommi

--- server/database/database.js
+++ server/database/database.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://postgres:123@localhost:3030/Board-Manager',{logging: false});
+const db = new Sequelize('postgres://postgres:123@localhost:5432/Board-Manager',{logging: false});
 
 module.exports = db;

305121aa0e98a1eb674c0b6457169132e9225d5d
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2019-06-20 22:57:12 -0400

    server config update

--- server/database/database.js
+++ server/database/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize({
+const db = new Sequelize({  
   connectionString:
     process.env.DATABASE_URL || 'postgres://postgres:123@localhost:3030/Board-Manager'
 }); 

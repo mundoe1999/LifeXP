f2c257e2b0c6954111cd00b204bf0a85345d1f62
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2019-06-21 01:47:43 -0400

    config update for db

--- server/database/database.js
+++ server/database/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const db = new Sequelize({
   connectionString:
-    process.env.DATABASE_URL || 'postgres://gcaiqfgnjfbqwi:989d3e7b4df895ffb39b995e6764744c4f5388f0bef2f0c7da84d127c8ca2b66@ec2-184-73-169-163.compute-1.amazonaws.com:5432/dde6qv5tqsan6o',
+    process.env.DATABASE_URL || 'postgres://postgres:123@localhost:3030/Board-Manager',
     logging: false,
     ssl: true,
     dialect: 'postgres'

49ff0c21766212ea30cc7ce3ea48cd9c5533a9f8
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2019-06-20 23:10:33 -0400

    troubleshooting db connect

--- server/database/database.js
+++ server/database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize({
   connectionString:
-    process.env.DATABASE_URL || 'postgres://postgres:123@localhost:3030/Board-Manager'
+    process.env.DATABASE_URL || 'postgres://postgres:123@localhost:3030/'
 }); 
 
 module.exports = db;
